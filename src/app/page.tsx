13fd071eb620c0a875a34ca97860104b6041d4cb
Marlon <xsantos.marlonx@gmail.com>
2025-07-22 16:15:00 -0300

    inciado o componente sobre mim

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,16 +25,16 @@ const Page = () => {
   return (
     <div className="bg-gray-950 rounded-md relative z-10 font-display">
 
-      <div className=""> {/* menu hamburger inicio */}
+      <div className="">
         <MenuMobile menu={listMobileMenu} handleClick={handleClick} />
-      </div> {/* menu hamburger inicio */}
+      </div>
 
       <div>
         <RedesAndLogo redeLogo={listRedesAndLogo} />
       </div>
 
 
-      <div className="bg-black container mx-auto shadow shadow-[#410085]">
+      <div className="bg-black container mx-auto shadow shadow-[#410085]"> {/* início header */}
         <Heade head={listHead} />
 
         <div className="px-2 flex justify-center items-center py-30">
@@ -44,7 +44,20 @@ const Page = () => {
         <div>
           <InfoTech infoTech={listInfoTech} />
         </div>
-      </div>
+
+        <div className="flex justify-around pt-40 pb-40 bg-gray-950">
+          <p>desc</p>
+
+          <div className="w-[320px] h-[320px] border rounded-md flex justify-center items-center">
+            <div className="flex flex-col gap-10"> 
+            img
+             <img src="" alt="" />
+             <p className="text-2xl">Olá, eu sou Marlon</p>
+            </div>
+          </div>
+        </div>
+
+      </div> {/* fim header */}
     </div>
   );
 }
