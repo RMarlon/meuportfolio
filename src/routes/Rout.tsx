f35cd79654db1249b83f6a389a74cf061e55451b
Marlon <xsantos.marlonx@gmail.com>
2025-07-31 09:44:07 -0300

    iniciando configuração de das rotas na página

---
+++ src/routes/Rout.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const Rout = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route>
+                    
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
+}
