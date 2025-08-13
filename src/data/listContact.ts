a309153377c78757c1f947a80accba864b428c46
Marlon <xsantos.marlonx@gmail.com>
2025-08-13 11:38:22 -0300

    feito uma correção na sessão de contact

--- src/data/listContact.ts
+++ src/data/listContact.ts
@@ -2,7 +2,7 @@ import { Contact } from "@/types/Contact";
 
 export const listContact: Contact[] = [
     {
-        contactMe: ['Ente em', 'contato'],
+        contactMe: ['Entre em', 'contato'],
         email: ['https://mail.google.com/mail/u/0/#inbox', 'assets/email.svg', 'xsantos.marlonx@gmail.com'],
         linkedin: ['https://www.linkedin.com/in/marlon-santos-497b9134a/', 'assets/linkedin.svg', 'Linkedin'],
         instagram:['https://www.instagram.com/marlonsantosdev/', 'assets/instagram.svg', '@marlonsantosdev'],
